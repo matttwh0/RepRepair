de67fc6dcdb87d562fb2c059cf61b9fecf540365
matttwh0 <matttran2004@gmail.com>
2024-10-19 16:42:59 -0700

    added navbar, changed color theme

---
+++ src/app/components/NavBar.tsx
@@ -0,0 +1,18 @@
+// Navbar.tsx
+import React from 'react';
+
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      
+      <div>
+        <h2><a href="#home">Home</a></h2>
+        
+        <h2><a href="#contact">Contact</a></h2>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,10 @@
 
 import React from 'react';
 import { useRouter } from 'next/navigation';
+//import Image from 'next/image';
+import Navbar from './components/NavBar'; // Import Navbar component
 import './globals.css';
+import liftingImage from './images/lifting.jpeg';
 
 function HomePage() {
   const router = useRouter();
@@ -11,8 +14,6 @@ function HomePage() {
     const file = event.target.files?.[0];
     if (file) {
       console.log('Video uploaded:', file.name);
-      // Redirect to the second page after uploading
-      router.push('/second-page');
     }
   };
 
@@ -21,21 +22,24 @@ function HomePage() {
   };
 
   return (
-    <div className="homepage-container">
-      <div className="card">
-        <h1 className="title">RepRepair</h1>
-        <button className="upload-button" onClick={handleClick}>Upload Video</button>
-        <input
-          id="fileInput"
-          type="file"
-          accept="video/*"
-          style={{ display: 'none' }}
-          onChange={handleFileUpload}
-        />
+    <>
+      <Navbar /> {}
+      <div className="homepage-container">
+        {/* <Image src={liftingImage} alt="Lifting" className="lifting-image" width={400} height={300} /> */}
+        <div className="card">
+          <h1 className="title">RepRepair</h1>
+          <button className="upload-button" onClick={handleClick}>Upload Video</button>
+          <input
+            id="fileInput"
+            type="file"
+            accept="video/*"
+            style={{ display: 'none' }}
+            onChange={handleFileUpload}
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
 export default HomePage;
-
